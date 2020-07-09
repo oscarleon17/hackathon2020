e6af50a01336deb7cb98fafad01421cabb42cf1b
oscarleon17 <oscar.leon@louisville.edu>
2020-07-09 15:37:05 -0400

    Initial Commit

---
+++ android/app/src/main/kotlin/nationwide/nastyhackapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package nationwide.nastyhackapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:nastyhackapp/services/authentication.dart';
+import 'package:nastyhackapp/pages/root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Nationwide Health App',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
